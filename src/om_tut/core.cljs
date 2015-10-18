6269313f805c451913ab5b0c0959cd3a8f6d9396
Ryan Mulcahy <ryan.mulcahy@code42.com>
2015-10-17 22:51:14 -0500

    Add more of the tutorial

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -4,24 +4,47 @@
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
+(println "Edits to this text should show up in your developer console!")
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom
+                    {:contacts
+                     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+                      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+                      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+                      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+                      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+                      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
 
-(om/root
-  (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text data)))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
 
+(defn display-name [{:keys [fist last] :as contact}]
+  (str last ", " first (middle-name contact)))
+
+(defn contact-view [contact owner]
+  (reify
+      om/IRender
+    (render [this]
+      (dom/li nil (display-name contact)))))
+
+(defn contacts-view [data owner]
+  (reify
+      om/IRender
+    (render [this]
+      (dom/div nil
+               (dom/h2 nil "Contact list")
+               (apply dom/ul nil
+                      (om/build-all contact-view (:contacts data)))))))
+
+(om/root contacts-view app-state
+  {:target (. js/document (getElementById "contacts"))})
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
-
